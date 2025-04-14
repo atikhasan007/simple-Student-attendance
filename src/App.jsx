fdba20644ddcbbfc2beaadd6d926a17ff0fc4a5f
atik007 <atikhasan1971arik@gmail.com>
2025-04-14 09:58:46 +0600

    using useReducer and argument of dispatch is string

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,51 @@ import StudentFrom from './components/StudentFrom';
 import StudentSection from './components/StudentSection';
 import StudentProvider from './context/Student';
 
+import { useReducer } from 'react';
+
+const counterReducer = (state, action)=>{
+  // if(action === "increase"){
+  //    return state + 1;
+  // }
+  // else{
+  //   return state - 1;
+  // }
+
+
+
+  switch(action){
+    case  "increase" :
+      return state + 1;
+    case "decrease" :
+      return state - 1;
+
+  }
+}
+
 const App = () => {
-  return (
-    <StudentProvider>
-         <div className='App'> 
-        <StudentFrom />
-        <StudentSection />
-    </div>
-    </StudentProvider>
+  // return (
+  //   <StudentProvider>
+  //        <div className='App'> 
+  //       <StudentFrom />
+  //       <StudentSection />
+  //   </div>
+  //   </StudentProvider>
    
+  // )
+
+
+
+  const [counter, dispatch] = useReducer( counterReducer , 10);
+
+
+
+  return (
+    <div className='App'>
+      <p>the value of counter is {counter}</p>
+
+      <button onClick={()=>dispatch("increase")}>increase by 1</button>
+      <button onClick={()=>dispatch("decrease")}>decrease by 1</button>
+    </div>
   )
 }
 
